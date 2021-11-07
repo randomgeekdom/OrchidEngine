d65461b31264103c6feb68be214803bb8ba2a949
Anthony Jones <randomgeekdom@gmail.com>
2021-11-07 15:38:26 -0600

    Starting the orchid cavalry

---
+++ OrchidCavalry/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using OrchidCavalry.ViewModels;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace OrchidCavalry
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            this.DataContext = new MainViewModel();
+        }
+
+
+    }
+}
---
+++ OrchidCavalry/ViewModels/MainViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OrchidCavalry.ViewModels
+{
+    public class MainViewModel
+    {
+        private readonly Selection selection = new Selection();
+        public string Title => this.selection.Title;
+        public string Body => this.selection.Body;
+
+        public Dictionary<string, Func<Selection>> Options => this.selection.Options;
+    }
+
+    public class Selection
+    {
+        public string Title { get; set; } = "This is the title";
+        public string Body { get; set; } = "There is more to life than this";
+
+        public Dictionary<string, Func<Selection>> Options { get; set; } = new Dictionary<string, Func<Selection>> {
+            {"hello", ()=>null },
+            {"goodbye", ()=>null },
+        };
+    }
+}
